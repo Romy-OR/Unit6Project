09c5709a84de24f4b0a50ead187aca7bce477129
Romy-OR <131908@epsstudents.org>
2025-01-21 14:31:03 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        //CardGameGUI gui = new CardGameGUI(new ElevensBoard());
+        //gui.displayGame();
+        System.out.println("Hi!");
+    }
+}
