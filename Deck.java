d2787e6573c7eb606421117cce0cfc36ec25b300
Romy-OR <131908@epsstudents.org>
2025-01-27 01:32:07 -0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,1 +1,18 @@
+public class Deck{
+    Card[] cards = new Card[52];
+    int size;
+    
+    public Deck(String[] ranks, String[] suits, int[] points){
+        Card[] deck = new Card[ranks.length * suits.length];
+        int count = 0;
+        for (int i = 0; i < suits.length; i++){
+            for (int j = 0; j < ranks.length; j++){
+                Card ncard = new Card(ranks[j], suits[i], points[j]);
+                deck[count] = ncard;
+                count++;
+            }
+        }
+        size = deck.length;
+    }
+}
 
