1176f486ab68e2e8086b7e78d60bae45340ade02
Romy-OR <131908@epsstudents.org>
2025-01-28 09:58:06 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ public class Main{
     public static void main(String[] args){
         //CardGameGUI gui = new CardGameGUI(new ElevensBoard());
         //gui.displayGame();
-        String[] ranks1 = {"A", "B", "C"};
-        String[] suits1 = {"Giraffes", "Lions"};
-        int[] points1 = {2, 1, 6};
-        Deck d1 = new Deck(ranks1, suits1, points1);
-        for (Card c1 : d1){
-            System.out.println(c1);
-        }
+        String[ ] ranks = {"A", "K", "Q"};
+        String[ ] suits = {"diamond", "heart"};
+        int[ ] pointValues = {1, 2, 3};
+        Deck d1 = new Deck(ranks, suits, pointValues);
+        d1.deal();
+        d1.deal();
+
+        System.out.println(d1);
     }
 }
